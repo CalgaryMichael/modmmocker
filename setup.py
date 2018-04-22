4bc5a3c6e08abe4763e5666d78d07c16ff63251e
Calgary Michael <cseth.michael@gmail.com>
2018-04-21 20:16:07 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+import codecs
+from setuptools import setup
+
+__author__ = "Calgary Michael"
+__contact__ = "cseth.michael@gmail.com"
+__version__ = "0.0.1"
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='modmmocker',
+    version=__version__,
+    description='A Proof of Concept mocker for PyMODM',
+    long_description=long_description,
+    url='https://github.com/CalgaryMichael/modmmocker',
+    author=__author__,
+    author_email=__contact__,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: Apache License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='json api',
+    py_modules=["liquify"],
+    extras_require={
+        'test': ['mock'],
+    }
+)
